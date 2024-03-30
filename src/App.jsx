295bbbade0da568225a9867d732601f3b81df00e
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-03-30 22:18:15 +0200

    RC_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable no-inner-declarations */
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import "./App.css";
 import { requestProductsByQuery } from "./services/api";
 import SearchBar from "./components/SearchBar/SearchBar";
@@ -23,31 +22,36 @@ function App() {
   // searchBox
   const onSubmit = (eventValue) => {
     setSearchImage(eventValue);
-    setCurrentPage(1); 
-    setImagesData([]);
+    setCurrentPage(1);
   };
 
-  //request
-  useEffect(() => {
+  // Memoized function to handle requestProductsByQuery
+  const fetchData = useMemo(() => async () => {
     if (searchImage) {
-      async function fetchProductsByQuery() {
-        try {
-          setIsLoad(true);
-          const data = await requestProductsByQuery(searchImage, IMAGE_PER_PAGE, currentPage);
-          setImagesData(prevImagesData => [...prevImagesData, ...data.results]); 
-          setTotalImageOnApi(data.total);
-        } catch (error) {
-          setIsError(true);
-        } finally {
-          setIsLoad(false);
-        }
+      try {
+        setIsLoad(true);
+        const data = await requestProductsByQuery(searchImage, IMAGE_PER_PAGE, currentPage);
+        setImagesData(prevImagesData => [...prevImagesData, ...data.results]); 
+        setTotalImageOnApi(data.total);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoad(false);
       }
-  
-      fetchProductsByQuery();
     }
   }, [searchImage, currentPage]);
 
-  //modal
+  // Clear ImagesData when searchImage changes
+  useEffect(() => {
+    setImagesData([]);
+  }, [searchImage]);
+
+  // request
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  // modal
   const onClickOnImage = (imageUrl) => {
     setSelectedImageUrl(imageUrl);
     setModalIsOpen(true);
@@ -57,9 +61,9 @@ function App() {
     setModalIsOpen(false);
   };
 
-  //load more
+  // load more
   const onClickLoadMore = () => {
-    setCurrentPage((prevPage) => prevPage + 1); 
+    setCurrentPage(prevPage => prevPage + 1); 
   };
 
   return (
