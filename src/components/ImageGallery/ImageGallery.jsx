1ab4384cca33074e564486f7bde3734953e12e8e
TrotskyuYurii <tro2001@gmail.com>
2024-03-31 18:57:23 +0300

    fix_key_loadMore

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,15 +2,12 @@ import ImageCard from "../ImageCard/ImageCard";
 import css from "../ImageGallery/ImageGallery.module.css";
 
 const ImageGallery = ({ Images, onClickOnImage }) => {
-  // Внутрішній лічильник для генерації унікальних ключів (була постіно проблема унікальниіх ключів при повторному натисканні серач після натискання лоадМоре)
-  let keyCounter = 0;
-
   return (
     <ul className={css.ImageGalleryUl}>
       {Images.map((image) => (
-        <li className={css.ImageCardLi} key={getKey()}>
+        <li className={css.ImageCardLi} key={image.id}>
           <ImageCard
-            key={getKey()} // Унікальний ключ для кожного ImageCard
+            key={image.id} 
             smallImage={image.urls.small}
             alt_description={image.alt_description}
             onClickOnImage={onClickOnImage}
@@ -20,11 +17,6 @@ const ImageGallery = ({ Images, onClickOnImage }) => {
       ))}
     </ul>
   );
-
-  // Функція для генерації унікального ключа
-  function getKey() {
-    return keyCounter++;
-  }
 };
 
 export default ImageGallery;
